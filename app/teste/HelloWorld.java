1471853af73259db889832df1d97271ef3f58aa2
HumbertoSM <hsmgondim@gmail.com>
2014-06-07 16:00:35 -0300

    Testando traduzido

--- app/teste/HelloWorld.java
+++ app/teste/HelloWorld.java
@@ -3,7 +3,7 @@ package teste;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Brazil");
+		System.out.println("Olá Brasil");
 //testando as diferenças
 	}
 
