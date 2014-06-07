7a6027b75f2d0211cbe16587dff7c6b18423ab09
HumbertoSM <hsmgondim@gmail.com>
2014-06-07 14:50:56 -0300

    Testando

--- app/teste/HelloWorld.java
+++ app/teste/HelloWorld.java
@@ -3,7 +3,7 @@ package teste;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Fala galera!");
 //testando as diferenças
 	}
 
