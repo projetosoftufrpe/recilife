74a4937f5a88b1ca605c2a89bacb630e2e7c7cd0
projetosoftufrpe <alexsandro_abs@hotmail.com>
2014-06-24 22:51:59 -0300

    Testes na estrutura geral.

---
+++ app/controllers/VacinacaoController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import models.Total;
+import models.vacinacao.postos;
+import static models.Total.*;
+import static models.vacinacao.postos.*;
+import static play.data.Form.form;
+
+import play.Logger;
+import play.data.DynamicForm;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+
+public class VacinacaoController extends Controller {
+
+	
+	
+	
+}
